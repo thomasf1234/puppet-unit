24fb2da44fbd8aaf2cc475af94df9266adad732e
Thomas F <thomas.fisher@footballpools.com>
2018-04-08 18:23:28 +0100

    [#0] TF : adding Lock documentation

--- lib/puppet-unit/lock.rb
+++ lib/puppet-unit/lock.rb
@@ -4,21 +4,75 @@ module PuppetUnit
   class Lock
     include PuppetUnit::UnixTimestampUtils
 
-    attr_reader :uuid, :expires_at_unix_timestamp
-
+    # ==== Description
+    #
+    # class constructor
+    #
+    # ==== Signature
+    #
+    # @author thomasf1234
+    # @arg1   [String]    a uuid
+    # @arg2   [Integer]   expiry time of the lock as a unix timestamp
     def initialize(lock_uuid, expires_at_unix_timestamp)
       @uuid = lock_uuid
       @expires_at_unix_timestamp = expires_at_unix_timestamp
     end
 
+    # ==== Description
+    #
+    # the uuid assigned during initialization
+    #
+    # ==== Signature
+    #
+    # @author thomasf1234
+    # @return [String]                    the uuid assigned
+    # @see    PuppetUnit::Lock#initialize
+    attr_reader :uuid
+
+    # ==== Description
+    #
+    # the unix timestamp representing the lock expiry time, assigned during
+    # initialization
+    #
+    # ==== Signature
+    #
+    # @author thomasf1234
+    # @return [Integer]                   expiry time as unix timestamp
+    # @see    PuppetUnit::Lock#initialize
+    attr_reader :expires_at_unix_timestamp
+
+    # ==== Description
+    #
+    # the time that the lock expires in 'Time' format
+    #
+    # ==== Signature
+    #
+    # @author thomasf1234
+    # @return [Time]      the expiry time
     def expires_at
       Time.at(@expires_at_unix_timestamp)
     end
 
+    # ==== Description
+    #
+    # if the lock is still active
+    #
+    # ==== Signature
+    #
+    # @author thomasf1234
+    # @return [Boolean]   true if the lock has not yet expired, false otherwise
     def active?
       current_unix_timestamp < @expires_at_unix_timestamp
     end
 
+    # ==== Description
+    #
+    # if the lock has expired
+    #
+    # ==== Signature
+    #
+    # @author thomasf1234
+    # @return [Boolean]   true if the lock has expired, false otherwise
     def expired?
       !active?
     end
